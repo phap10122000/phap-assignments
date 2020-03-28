e0a759ad4397bb4c19ec7d0a15490cf995a5a017
phap10122000 <trinhhaphap@gmail.com>
2020-03-28 14:51:18 +0700

    17 binaries uploaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GameComponent } from './game/game.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { RequirementsComponent } from './requirements/requirements.component';
 
 
 
@@ -9,11 +13,11 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent}
-  // { path: 'add', component: ProductAddComponent},
-  // { path: 'about', component: AboutComponent},
-  // { path: 'list', component: ProductListComponent},
-  // { path: 'service', component: ServiceComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'game', component: GameComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'list', component: ProductListComponent},
+  { path: 'Rrequirement', component: RequirementsComponent}
   // { path: 'contact', component: ContactComponent},
   // { path: 'manager', component: ProductManagerComponent},
   // { path: 'manager/edit/:id', component: ProductEditComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
-import { MenComponent } from './men/men.component';
-import { WomenComponent } from './women/women.component';
-import { KidzComponent } from './kidz/kidz.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductSlideComponent } from './product-slide/product-slide.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ListService } from './list.service';
+import { AboutComponent } from './about/about.component';
+import { GameComponent } from './game/game.component';
+import { AdminComponent } from './admin/admin.component';
+import { RequirementsComponent } from './requirements/requirements.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, MenComponent, WomenComponent, KidzComponent, ProductListComponent, ProductSlideComponent, ProductDetailComponent ],
+  imports:      [ BrowserModule, FormsModule,AppRoutingModule,NgbModule ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, ProductListComponent, ProductSlideComponent, ProductDetailComponent, AboutComponent, GameComponent, AdminComponent, RequirementsComponent, TeamComponent, ContactComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ListService]
 })
