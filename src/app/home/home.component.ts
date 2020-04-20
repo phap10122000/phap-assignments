b2824a0915bd8945a48793f6944b17897038a1b3
phap10122000 <trinhhaphap@gmail.com>
2020-04-20 16:04:29 +0700

    4 files modified, 1 added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { from } from 'rxjs';
+import { Product } from '../Product';
+import { ProductService } from '../product.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+    products: Product[];
+  selected: Product;
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.getProducts();
   }
-
+  getProducts() {
+    this.productService.listProducts().subscribe(data =>{
+      this.products = data;
+    })
+    // this.products = this.productService.listProducts();
+  }
 }
