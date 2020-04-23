364fce450857ad0899ceae62d5c215f422af7364
phap10122000 <trinhhaphap@gmail.com>
2020-04-23 14:39:10 +0700

    3 files modified, 1 added

--- src/app/admin/admin-add/admin-add.component.ts
+++ src/app/admin/admin-add/admin-add.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../Product';
 import { ProductService } from '../product.service';
 import { ActivatedRoute, Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-admin-add',
   templateUrl: './admin-add.component.html',
@@ -9,15 +10,42 @@ import { ActivatedRoute, Router } from "@angular/router";
 })
 export class AdminAddComponent implements OnInit {
   product:Product = new Product;
+    registerForm: FormGroup;
+    submitted = false;
   constructor(private productService: ProductService,
     private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.validateEmail();
   }
    addProducts() {
     this.productService.addProducts(this.product).subscribe(data =>
     this.router.navigate(['/admin/manager']));
   }
-  
+
+  validateEmail(){
+     this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required]
+        }, {
+            validator: MustMatch('password', 'confirmPassword')
+        });
+  }
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+    }
 }
