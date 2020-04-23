d1cb787a9adb43318b7ac9bf304d16e11ad74e33
phap10122000 <trinhhaphap@gmail.com>
2020-04-23 14:49:06 +0700

    2 files modified

--- src/app/admin/admin-add/admin-add.component.ts
+++ src/app/admin/admin-add/admin-add.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../Product';
 import { ProductService } from '../product.service';
 import { ActivatedRoute, Router } from "@angular/router";
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-admin-add',
   templateUrl: './admin-add.component.html',
@@ -18,34 +19,34 @@ export class AdminAddComponent implements OnInit {
     private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.validateEmail();
+    // this.validateEmail();
   }
    addProducts() {
     this.productService.addProducts(this.product).subscribe(data =>
     this.router.navigate(['/admin/manager']));
   }
 
-  validateEmail(){
-     this.registerForm = this.formBuilder.group({
-            firstName: ['', Validators.required],
-            lastName: ['', Validators.required],
-            email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required, Validators.minLength(6)]],
-            confirmPassword: ['', Validators.required]
-        }, {
-            validator: MustMatch('password', 'confirmPassword')
-        });
-  }
+  // validateEmail(){
+  //    this.registerForm = this.formBuilder.group({
+  //           firstName: ['', Validators.required],
+  //           lastName: ['', Validators.required],
+  //           email: ['', [Validators.required, Validators.email]],
+  //           password: ['', [Validators.required, Validators.minLength(6)]],
+  //           confirmPassword: ['', Validators.required]
+  //       }, {
+  //           validator: MustMatch('password', 'confirmPassword')
+  //       });
+  // }
     get f() { return this.registerForm.controls; }
 
-    onSubmit() {
-        this.submitted = true;
+    // onSubmit() {
+    //     this.submitted = true;
 
-        // stop here if form is invalid
-        if (this.registerForm.invalid) {
-            return;
-        }
+    //     // stop here if form is invalid
+    //     if (this.registerForm.invalid) {
+    //         return;
+    //     }
 
-        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
-    }
+    //     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+    // }
 }
